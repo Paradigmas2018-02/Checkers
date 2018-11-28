cfcf25b4b99084f4e2d87f20713f6fd17e292374
rodrigocam <rodrigo.redcode@gmail.com>
2018-11-28 14:25:07 -0200

    Create some conversation constants

---
+++ src/agents/ConversationConstants.java
@@ -0,0 +1,9 @@
+package agents;
+
+public class ConversationConstants {
+	public final static String PLAYER_TO_PLAYER = "Sou um Player!";
+	public final static String PLAYER_1_NAME = "PLAYER 1";
+	public final static String PLAYER_2_NAME = "PLAYER 2";
+	public final static String PLAYER_TYPE = "CHECKER_PLAYER";
+	public final static String PLAY = "Sua vez!";
+}
